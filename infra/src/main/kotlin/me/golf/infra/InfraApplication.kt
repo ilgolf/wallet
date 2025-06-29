f2b9de20d33412e693e71180674311cfc0523e11
ilgolf <ilgolc@naver.com>
2025-06-29 21:32:06 +0900

    feat. initial commit

---
+++ infra/src/main/kotlin/me/golf/infra/InfraApplication.kt
@@ -0,0 +1,11 @@
+package me.golf.infra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfraApplication
+
+fun main(args: Array<String>) {
+    runApplication<InfraApplication>(*args)
+}
